c8b755449b9c73302842e285f52e56905ceda081
Tmoradi <tiam.moradi34@gmail.com>
2022-08-25 22:03:27 -0400

    Finished assignment 3.

---
+++ assignments/week-3-reddit-sentiment/secrets_reddit.py
@@ -0,0 +1,4 @@
+# Here we are going to store our api keys and tokens
+REDDIT_API_CLIENT_ID = "__jWqPKJlmoBnc25YJ0guw"
+REDDIT_API_CLIENT_SECRET = "v4A7xjlCnkr5vhQhL-JGa1x1Z3fVkg"
+REDDIT_API_USER_AGENT = "User Agent for getting reddit comments"
