6738f9148984dfa6d1e798dee297a5c281128816
Tmoradi <tiam.moradi34@gmail.com>
2022-08-27 20:23:46 -0400

    Need to remove secrets for gitignore to work.

--- assignments/week-3-reddit-sentiment/secrets_reddit.py
+++
@@ -1,4 +0,0 @@
-# Here we are going to store our api keys and tokens
-REDDIT_API_CLIENT_ID = "__jWqPKJlmoBnc25YJ0guw"
-REDDIT_API_CLIENT_SECRET = "v4A7xjlCnkr5vhQhL-JGa1x1Z3fVkg"
-REDDIT_API_USER_AGENT = "User Agent for getting reddit comments"
